9d033cf7f8688197aae1696d7e45fcf6b884ad79
GerardoDev25 <GerardoDev25@gmail.com>
2021-07-16 17:26:41 -0400

    finish the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,67 @@
 const htmlWebPackPlugin = require("html-webpack-plugin");
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
 
+// ? MODULES
+const jsModule = {
+   test: /\.js$/i,
+   exclude: /node_modules/,
+   use: {
+      loader: "babel-loader",
+   },
+};
+
+const htmlModule = {
+   test: /\.html$/i,
+   use: [
+      {
+         loader: "html-loader",
+         options: {
+            minimize: true,
+         },
+      },
+   ],
+};
+
+const cssMudule = {
+   test: /\.css$/i,
+   use: [
+      {
+         loader: miniCssExtractPlugin.loader,
+         options: {
+            publicPath: "./",
+         },
+      },
+      "css-loader",
+   ],
+};
+
+const fontMudule = {
+   test: /\.(woff|woff2|eot|ttf|otf)$/i,
+   use: ["file-loader?name=assets/[name].[ext]"],
+};
+
+// ? PLUGINS
+
+const htmlPlugin = new htmlWebPackPlugin({
+   template: "./src/index.html",
+   filename: "./index.html",
+   chunks: ["js"],
+   hash: true,
+});
+
+const miniCssPlugin = new miniCssExtractPlugin();
+
+// ! *********** modules expert -----------------
 module.exports = {
    entry: {
       js: "./src/index.js",
-      react: "./src/index_react.js",
-      ts: "./src/index_ts.js",
    },
    output: {
       filename: "[name].[chunkhash].js",
    },
    module: {
-      rules: [
-         {
-            test: /\.jsx?$/i,
-            exclude: /node_modules/,
-            use: {
-               loader: "babel-loader",
-            },
-         },
-
-         {
-            test: /\.tsx?$/i,
-            exclude: /node_modules/,
-            use: {
-               loader: "ts-loader",
-            },
-         },
-
-         {
-            test: /\.html$/i,
-            use: [
-               {
-                  loader: "html-loader",
-                  options: {
-                     minimize: true,
-                  },
-               },
-            ],
-         },
-
-         {
-            test: /\.css$/i,
-            // use: [miniCssExtractPlugin.loader, "css-loader"],
-            use: [
-               {
-                  loader: miniCssExtractPlugin.loader,
-                  options: {
-                     publicPath: "./",
-                  },
-               },
-               "css-loader",
-            ],
-         },
-
-         {
-            test: /\.(jpe?g|png|gif|svg|webp)$/i,
-            use: [
-               "file-loader?name=assets/[name].[ext]",
-               "image-webpack-loader",
-            ],
-         },
-
-         {
-            test: /\.(woff)$/i,
-            use: ["file-loader?name=assets/[name].[ext]"],
-         },
-      ],
+      rules: [jsModule, htmlModule, cssMudule, fontMudule],
    },
-   plugins: [
-      // new htmlWebPackPlugin({
-      //    template: "./src/index.html",
-      //    filename: "./index.html",
-      // }),
-      new htmlWebPackPlugin({
-         template: "./src/index.html",
-         filename: "./index.html",
-         chunks: ["js"],
-         hash: true,
-      }),
-      new htmlWebPackPlugin({
-         template: "./src/index.html",
-         filename: "./react.html",
-         chunks: ["react"],
-         hash: true,
-      }),
-      new htmlWebPackPlugin({
-         template: "./src/index.html",
-         filename: "./ts.html",
-         chunks: ["ts"],
-         hash: true,
-      }),
 
-      new miniCssExtractPlugin(),
-   ],
+   plugins: [htmlPlugin, miniCssPlugin],
 };
